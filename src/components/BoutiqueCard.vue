7edd8c5e84b796a8b2695f470e8c8994702f889d
Giacomo Bortolotti <bellaj4ck@gmail.com>
2019-06-18 16:08:54 +0200

    Add Boutiques Archive

--- src/components/BoutiqueCard.vue
+++ src/components/BoutiqueCard.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
-    
-  </div>
+  <article class="boutique-card">
+    <header>
+      <h3>{{boutique.post_title}}</h3>
+    </header>
+    <div class="boutique-card__box">
+      <div class="boutique-card__address">{{boutique["wpcf-yoox-store-address"]}}</div>
+      <div class="boutique-card__book">
+        <router-link to="/">Book an appointment</router-link>
+      </div>
+    </div>
+  </article>
 </template>
 <script>
 export default {
-  
+  props: ["boutique"]
 }
 </script>
--- src/views/BoutiquesArchive.vue
+++ src/views/BoutiquesArchive.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    Archive
+    <header>
+      <h2>Archive</h2>
+    </header>
+    <div class="something">
+      <Card v-for="boutique in boutiques" :key="boutique.ID" :boutique="boutique" />
+    </div>
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
+import Card from "../components/BoutiqueCard.vue";
 export default {
-  
+  computed: {
+    ...mapGetters({
+      boutiques: 'bookable'
+    })
+  },
+  components: {
+    Card
+  }
 }
 </script>
